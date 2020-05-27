728bd1bada5b86a220aa8aac1362f8291657d757
sanrentai <sanrentai@163.com>
2020-05-27 16:03:33 +0800

    init

---
+++ cny_test.go
@@ -0,0 +1,9 @@
+package cny
+
+import "testing"
+
+func TestConvert(t *testing.T) {
+	//贰佰零贰万零肆元壹角壹分
+	//贰佰零贰万零肆元壹角壹分
+	t.Log(Convert(2020004.11))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sanrentai/cny
+
+go 1.14
