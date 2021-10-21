df66687fab1ff0667e230294026ee423733bbf6c
Sany <sanrentai@163.com>
2021-10-21 16:46:44 +0800

    修正负数，增加测试用例

--- cny.go
+++ cny.go
@@ -1,52 +1,52 @@
 package cny
 
 import (
-	"log"
 	"regexp"
 	"strconv"
 )
 
 func Convert(num float64) string {
-	strnum := strconv.FormatFloat(num*100, 'f', 0, 64)
+	money := num
+	if num < 0 {
+		money = -num
+	}
+	strnum := strconv.FormatFloat(money*100, 'f', 0, 64)
 	sliceUnit := []string{"仟", "佰", "拾", "亿", "仟", "佰", "拾", "万", "仟", "佰", "拾", "元", "角", "分"}
-	s := sliceUnit[len(sliceUnit)-len(strnum) : len(sliceUnit)]
+	s := sliceUnit[len(sliceUnit)-len(strnum):]
 	upperDigitUnit := map[string]string{"0": "零", "1": "壹", "2": "贰", "3": "叁", "4": "肆", "5": "伍", "6": "陆", "7": "柒", "8": "捌", "9": "玖"}
 	str := ""
 	for k, v := range strnum[:] {
 		str = str + upperDigitUnit[string(v)] + s[k]
 	}
-	reg, err := regexp.Compile(`零角零分$`)
+	reg, _ := regexp.Compile(`零角零分$`)
 	str = reg.ReplaceAllString(str, "整")
 
-	reg, err = regexp.Compile(`零角`)
+	reg, _ = regexp.Compile(`零角`)
 	str = reg.ReplaceAllString(str, "零")
 
-	reg, err = regexp.Compile(`零分$`)
+	reg, _ = regexp.Compile(`零分$`)
 	str = reg.ReplaceAllString(str, "整")
 
-	reg, err = regexp.Compile(`零[仟佰拾]`)
+	reg, _ = regexp.Compile(`零[仟佰拾]`)
 	str = reg.ReplaceAllString(str, "零")
 
-	reg, err = regexp.Compile(`零{2,}`)
+	reg, _ = regexp.Compile(`零{2,}`)
 	str = reg.ReplaceAllString(str, "零")
 
-	reg, err = regexp.Compile(`零亿`)
+	reg, _ = regexp.Compile(`零亿`)
 	str = reg.ReplaceAllString(str, "亿")
 
-	reg, err = regexp.Compile(`零万`)
+	reg, _ = regexp.Compile(`零万`)
 	str = reg.ReplaceAllString(str, "万")
 
-	reg, err = regexp.Compile(`零*元`)
+	reg, _ = regexp.Compile(`零*元`)
 	str = reg.ReplaceAllString(str, "元")
 
-	reg, err = regexp.Compile(`亿零{0, 3}万`)
+	reg, _ = regexp.Compile(`亿零{0, 3}万`)
 	str = reg.ReplaceAllString(str, "^元")
 
-	reg, err = regexp.Compile(`零元`)
+	reg, _ = regexp.Compile(`零元`)
 	str = reg.ReplaceAllString(str, "零")
-	if err != nil {
-		log.Fatal(err)
-	}
 	if num < 0 {
 		str = "负" + str
 	}
--- cny_test.go
+++ cny_test.go
@@ -1,9 +1,49 @@
 package cny
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestConvert(t *testing.T) {
-	//贰佰零贰万零肆元壹角壹分
-	//贰佰零贰万零肆元壹角壹分
-	t.Log(Convert(2020004.11))
+	type args struct {
+		num float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "整百",
+			args: args{202000.00},
+			want: "贰拾万贰仟元整",
+		},
+		{
+			name: "整元",
+			args: args{2020004.00},
+			want: "贰佰零贰万零肆元整",
+		},
+		{
+			name: "整角",
+			args: args{2020004.60},
+			want: "贰佰零贰万零肆元陆角整",
+		},
+		{
+			name: "正数",
+			args: args{2020004.11},
+			want: "贰佰零贰万零肆元壹角壹分",
+		},
+		{
+			name: "负数",
+			args: args{-2020004.11},
+			want: "负贰佰零贰万零肆元壹角壹分",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Convert(tt.args.num); got != tt.want {
+				t.Errorf("Convert() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
